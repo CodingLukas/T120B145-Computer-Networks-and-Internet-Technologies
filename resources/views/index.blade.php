810c703f2da37e69db492f367dc8b55f056b8f99
Lukas <lukasimbm@gmail.com>
2020-11-15 11:56:31 +0200

    show event name while making reservation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
             <div class="row">
                 <div class="col-lg-12 margin-tb">
                     <div class="pull-left">
-                        <h2>Sektoriai</h2>
+                        <h2>Renginio "{{$event->name}}" laisvos vietos sektoriuose</h2>
                     </div>
                 </div>
             </div>
@@ -43,74 +43,79 @@
             </table>
 
 
-            <script>
-                var prices = [];
-                @foreach ($sectors as $sector)
-                    prices[prices.length] = {{ $sector->getPricePerSeat() }};
-                @endforeach
+            @if(Auth::user())
 
-                $(document).ready(function () {
-                    $('.sector').bind('keyup mouseup', function () {
-                        let totalAmount = calculateTotal();
-                        $('#totalPrice').html(totalAmount);
-                    });
-                })
+                <script>
+                    var prices = [];
+                    @foreach ($sectors as $sector)
+                        prices[prices.length] = {{ $sector->getPricePerSeat() }};
+                    @endforeach
 
-                function calculateTotal() {
-                    let total = 0.00;
-                    for (i = 0; i < prices.length; i++) {
-                        let amount = $('#sector-' + i).val();
-                        total += amount * prices[i];
+                    $(document).ready(function () {
+                        $('.sector').bind('keyup mouseup', function () {
+                            let totalAmount = calculateTotal();
+                            $('#totalPrice').html(totalAmount);
+                        });
+                    })
+
+                    function calculateTotal() {
+                        let total = 0.00;
+                        for (i = 0; i < prices.length; i++) {
+                            let amount = $('#sector-' + i).val();
+                            total += amount * prices[i];
+                        }
+                        return total;
                     }
-                    return total;
-                }
 
-            </script>
+                </script>
 
-            <style>
-                .table2 {
-                    margin: auto;
-                    width: 50% !important;
-                }
-            </style>
+                <style>
+                    .table2 {
+                        margin: auto;
+                        width: 50% !important;
+                    }
+                </style>
 
-            <form action="{{ route('tickets.create', ['eventId' => $event->id]) }}" method="POST">
-                @csrf
-                @method('POST')
+                <form action="{{ route('tickets.create', ['eventId' => $event->id]) }}" method="POST">
+                    @csrf
+                    @method('POST')
 
-                <div class="row justify-content-center">
+                    <div class="row justify-content-center">
 
-                    <div class="row">
-                        <div class="col-lg-12 margin-tb">
-                            <div class="pull-left">
-                                <h2>Rezervuoti bilietus renginiui: {{$event->name}}</h2>
+                        <div class="row">
+                            <div class="col-lg-12 margin-tb">
+                                <div class="pull-left">
+                                    <h2>Rezervuoti bilietus renginiui: "{{$event->name}}"</h2>
+                                </div>
                             </div>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                            <table class="table2 table-sm table-bordered table-responsive-sm">
+                                <tr>
+                                    <th>Sektoriaus numeris</th>
+                                    <th>Rezervuojamas kiekis</th>
+                                </tr>
+                                @php($i = 0)
+                                @foreach ($sectors as $sector)
+                                    <tr>
+                                        <td>{{ ++$i }}</td>
+                                        <td>
+                                            <input name="sector-{{$i-1}}" class="form-control sector" type="number"
+                                                   value="{{old('sector-'.($i-1)) ? old('sector-'.($i-1)) : 0}}" min="0"
+                                                   id="sector-{{$i-1}}">
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                            Iš viso kaina: <span id="totalPrice">0</span> EUR
+                            <button type="submit" class="btn btn-primary">Rezervuoti</button>
                         </div>
                     </div>
 
-                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                        <table class="table2 table-sm table-bordered table-responsive-sm">
-                            <tr>
-                                <th>Sektoriaus numeris</th>
-                                <th>Rezervuojamas kiekis</th>
-                            </tr>
-                            @php($i = 0)
-                            @foreach ($sectors as $sector)
-                                <tr>
-                                    <td>{{ ++$i }}</td>
-                                    <td>
-                                        <input name="sector-{{$i-1}}" class="form-control sector" type="number"
-                                               value="{{old('sector-'.($i-1)) ? old('sector-'.($i-1)) : 0}}" min="0" id="sector-{{$i-1}}">
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </table>
-                        Iš viso kaina: <span id="totalPrice">0</span> EUR
-                        <button type="submit" class="btn btn-primary">Rezervuoti</button>
-                    </div>
-                </div>
+                </form>
 
-            </form>
+            @endif
 
         @else
             Nėra aktyvaus renginio kurį galima būtų rezervuoti!
